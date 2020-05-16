9a26193ff5f1186470e1fcb82219c5ff203d9338
lloydM <lloydMwiti4@gmail.com>
2020-05-16 16:38:00 +0300

    initial commit

---
+++ js/index.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    var nav=$('nav');
+    $(window).bind('scroll',function(){
+
+        if ($(window).scrollTop() > 90){
+            $('.container-fluid').css('padding-top','70px');
+            $('.navbar').css('box-shadow','0 0.15rem .6rem -.2rem blue');
+            nav.addClass('stopScroll');
+        }else {
+            nav.removeClass('stopScroll');
+            $('.navbar').css('box-shadow','none');
+            $('.container-fluid').css('padding-top','0px');
+
+        }
+    })
+})
