337adcd4df35b2a9c63606c384744e7ee0bb28e0
lloydM <lloydMwiti4@gmail.com>
2020-05-17 13:45:53 +0300

    added cards to select size and topings

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     var nav=$('nav');
     $(window).bind('scroll',function(){
 
-        if ($(window).scrollTop() > 90){
+        if ($(window).scrollTop() > 70){
             $('.container-fluid').css('padding-top','70px');
             $('.navbar').css('box-shadow','0 0.15rem .6rem -.2rem blue');
             nav.addClass('stopScroll');
